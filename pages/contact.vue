c15487a614c4f547a381199ff65e7dcc3f55e182
Xifré Font <xifre.font.97@gmail.com>
2025-06-21 17:29:11 +0200

    PAGES: Add Contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,1 +1,41 @@
-<template><p>Contact</p></template>
+<script lang="ts" setup>
+const { t } = useI18n();
+const { contact } = useRuntimeConfig().public;
+
+const KEYS = ["description", "location", "contact", "instagram"];
+
+const textBlocks = KEYS.map(
+  (block) => t(`contact.${block}`, { email: contact }) as string
+);
+</script>
+
+<template>
+  <div class="contact">
+    <article class="contact__content">
+      <p v-for="block in textBlocks" :key="block" v-html="block" />
+    </article>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.contact {
+  position: absolute;
+  inset: 0;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__content {
+    max-width: 17rem;
+    display: flex;
+    flex-direction: column;
+    gap: $distances-s;
+    color: $color-primary;
+    text-align: center;
+    font-size: $font-size-m;
+  }
+}
+</style>
