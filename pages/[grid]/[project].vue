9f6cc76618a27fe27269fa179efc9ec9d82e9719
Xifré Font <xifre.font.97@gmail.com>
2025-06-21 17:30:21 +0200

    PAGES: Remove unused styles

--- pages/[grid]/[project].vue
+++ pages/[grid]/[project].vue
@@ -10,21 +10,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <section class="works">
+  <section>
     <img v-for="{ alt, src } in images.list.value" :key="src" :src :alt />
   </section>
 </template>
-
-<style lang="scss" scoped>
-.landing {
-  position: relative;
-
-  &__background {
-    position: absolute;
-    inset: 0;
-    height: 100vh;
-    width: 100vw;
-    object-fit: cover;
-  }
-}
-</style>
