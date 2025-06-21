10c38e85e50899218776844a3724400984ad87ef
Xifré Font <xifre.font.97@gmail.com>
2025-06-21 12:29:34 +0200

    MENU: Add navigation menu

---
+++ components/NavigationMenu.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { addBrackets } from "#imports";
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <nav class="navigation-menu">
+    <button type="button" class="navigation-menu__item">
+      {{ addBrackets(t("menu.menu") as string) }}
+    </button>
+
+    <NuxtLink to="/" class="navigation-menu__item">
+      {{ addBrackets(t("meta.title") as string) }}
+    </NuxtLink>
+
+    <NuxtLink to="contact" class="navigation-menu__item">
+      {{ addBrackets(t("menu.contact") as string) }}
+    </NuxtLink>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.navigation-menu {
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 2;
+  color: white;
+
+  &__item {
+    font-size: $font-size-m;
+    cursor: pointer;
+  }
+}
+</style>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,4 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt({
-  rules: { "vue/multi-word-component-names": "off" },
-});
+export default withNuxt();
